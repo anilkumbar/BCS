5842568e3c706f5a50f761a211a305e3f7291256
Anil Kumbar <akumbar3@gmail.com>
2021-02-15 16:24:22 +0530

    Commit for the first time

---
+++ src/main/java/NewTest.java
@@ -0,0 +1,13 @@
+
+public class NewTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Added new file for understanding git functionality
+		
+		System.out.println("Hello");
+
+	}
+
+}
